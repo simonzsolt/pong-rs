2091b26544593828c3da114b2eec9939dd13d4ba
Peter Czibik <p.czibik@gmail.com>
2019-03-11 21:34:10 +0100

    add scoring

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ mod pong;
 mod systems;
 
 use crate::pong::Pong;
-use amethyst::core::transform::TransformBundle;
-use amethyst::input::InputBundle;
-use amethyst::prelude::*;
-use amethyst::renderer::{
-    DisplayConfig, DrawFlat2D, Event, Pipeline, RenderBundle, Stage, VirtualKeyCode,
+
+use amethyst::{
+    core::transform::TransformBundle,
+    input::InputBundle,
+    prelude::*,
+    renderer::{DisplayConfig, DrawFlat2D, Event, Pipeline, RenderBundle, Stage, VirtualKeyCode},
+    ui::{DrawUi, UiBundle},
+    utils::application_root_dir,
 };
-use amethyst::utils::application_root_dir;
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
@@ -22,7 +24,8 @@ fn main() -> amethyst::Result<()> {
     let pipe = Pipeline::build().with_stage(
         Stage::with_backbuffer()
             .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
-            .with_pass(DrawFlat2D::new()),
+            .with_pass(DrawFlat2D::new())
+            .with_pass(DrawUi::new()),
     );
 
     let binding_path = format!("{}/resources/bindings_config.ron", application_root_dir());
@@ -33,6 +36,7 @@ fn main() -> amethyst::Result<()> {
         .with_bundle(RenderBundle::new(pipe, Some(config)).with_sprite_sheet_processor())?
         .with_bundle(TransformBundle::new())?
         .with_bundle(input_bundle)?
+        .with_bundle(UiBundle::<String, String>::new())?
         .with(systems::PaddleSystem, "paddle_system", &["input_system"])
         .with(systems::MoveBallsSystem, "ball_system", &[])
         .with(
